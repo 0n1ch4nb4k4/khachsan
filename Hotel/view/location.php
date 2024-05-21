7609fdb68b9612775e0a25708821b7996680e0ed
Glaydys <163386523+Glaydys@users.noreply.github.com>
2024-05-21 22:21:35 +0700

    Add files via upload

---
+++ Hotel/view/location.php
@@ -0,0 +1,34 @@
+<div class="title"><strong>CÁC ĐỊA ĐIỂM</strong></div>
+    <div class="locations">
+    <div class="row">
+        <div class="location">
+            <img src="img/DL.jpg">
+            <div class="overlay">Đà Lạt</div>
+        </div>
+        <div class="location">
+            <img src="img/HN.jpg">
+            <div class="overlay">Hà Nội</div>
+        </div>
+        <div class="location">
+            <img src="img/Tp.jpg">
+            <div class="overlay">TPHCM</div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="location">
+            <img src="img/DN.jpg">
+            <div class="overlay">Đà Nẵng</div>
+        </div>
+        <div class="location">
+            <img src="img/PT.jpg">
+            <div class="overlay">Phan Thiết</div>
+        </div>
+    </div>
+    </div>
+    
+    <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+<df-messenger
+  chat-title="ChatBot"
+  agent-id="f54c4432-1a0e-4841-b73b-a037c00211ca"
+  language-code="vi"
+></df-messenger>
